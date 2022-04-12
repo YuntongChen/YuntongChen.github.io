24013f3a078bf770032e9c5749e2bb08fb96e9cf
cheny409 <cheny409@newschool.edu>
2022-04-12 09:12:46 -0400

    Update Assignment9.js

--- Assignment9/Assignment9.js
+++ Assignment9/Assignment9.js
@@ -6,42 +6,42 @@ const tableUrl = "https://api.airtable.com/v0/appdX6TGwTTcHDg7D/Crystal";
 // URL with API Key Authentication
 const authenticatedUrl = tableUrl + "?api_key=" + airtableApiKey;
 
-// STEP 2: DOM REFERENCES
 const bodyElement = document.querySelector("main#app");
-
-// STEP 3: USING FUNCTIONS, VARIABLES, AND LOOPS
-
-// Get a 'Promise' Representing the Data that Will be Returned
+const CrystalContainerElement = document.querySelector("#Crystal")
 fetch(authenticatedUrl)
   // When 'Promise' is 'Resolved', get the JSON data
   .then((res) => res.json())
   // Use the JSON Data
   .then((data) => {
-    const illustrations = data.records;
-    for (let index = 0; index < crystal.length; index++) {
-      // Get Illustration
-      const illustration = crystal[index];
-      //   Get Title
-      const title = crystal.fields.Title;
-      // Get Image Url
-      const imageUrl = crystal.fields.Image[0].url;
-      // Create Container Object
-      const containerElement = document.createElement("div");
-      containerElement.classList.add("container");
-      // Create Image Element
-      const imageElement = document.createElement("img");
-      imageElement.classList.add( "image");
-      imageElement.setAttribute("src", imageUrl);
-      // Create Title Element
-      const titleElement = document.createElement("p");
-      titleElement.classList.add("title");
-      titleElement.innerHTML = title;
-      // Add Elements to DOM
-      bodyElement.appendChild(containerElement);
-      containerElement.appendChild(imageElement);
-      containerElement.appendChild(titleElement);
-    }
+    const records = data.records
+    for (let index = 0; index < records.length; index++) {
+        console.log(records[index])
+        const title = records[index].fields.Title
+        console.log(records[index].fields)
+        const imageUrl = records[index].fields.Images[0].url
+
+        const containerElement = document.createElement('div')
+        containerElement.classList.add('container')
+
+        const imageElement =document.createElement('img')
+        imageElement.classList.add('image')
+        imageElement.setAttribute('src', imageUrl)
+
+        const titleElement = document.createElement('p')    
+        titleElement.classList.add('title')
+        titleElement.innerHTML = title
+
+        CrystalContainerElement.appendChild(containerElement)
+
+        containerElement.appendChild(imageElement)
+
+        containerElement.appendChild(titleElement)
+
+ }
   })
   .catch((error) => {
     console.log(error);
   });
+  
+
+
