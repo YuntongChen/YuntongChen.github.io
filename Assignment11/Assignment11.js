5c9c7b76b76da57fcc8499dc7d6fd46366704bf1
cheny409 <cheny409@newschool.edu>
2022-04-30 13:43:56 -0400

    Update Assignment11.js

--- Assignment11/Assignment11.js
+++ Assignment11/Assignment11.js
@@ -8,7 +8,10 @@ formSubmitEventElement.addEventListener('submit', (event) => {
     const value = formInputElement.value
     const feedbackElement = document.createElement('ul')
     feedbackElement.innerHTML = "⊛" + value
-
+    // get random color
+    var color = colors[Math.floor(Math.random()*colors.length)] // geting random color from array;
+    // set color on new list item
+    feedbackElement.style.color = color
     formSubmitEventFeedbackElement.appendChild(feedbackElement)
     formInputElement.value = ""
 })
@@ -16,14 +19,17 @@ formSubmitEventElement.addEventListener('submit', (event) => {
 
 var feedback = document.getElementById("feedback");
 var words = feedback.innerText.split(" "); 
-var colors = ["#a7b8c9","#728a2a", "#d74d24", "#dca880", "#57342f", "#002dd2","#7295e0","white","#ebf298","#836aa5","black"];
-var ul = [];
+var colors = ["#728a2a", "#d74d24", "#57342f", "#282f3d","#493960","black"];
+// var ul = [];
+// const updateColors = () => {
+//     for(var x = 0; x < words.length; x++){
+//         var color = colors[Math.floor(Math.random()*colors.length)] // geting random color from array;
+//         console.log(color)
+//         var ul = "<ul style='color: " + color + ";'>" + words[x] + "</ul>"
+        
+//         // ul.push(ul);
+//     }
+// }
 
-for(var x = 0; x < words.length; x++){
-    var color = colors[Math.floor(Math.random()*colors.length)] // geting random color from array;
-    var ul = "<ul style='color: " + color + ";'>" + words[x] + "</ul>"
-    
-    ul.push(ul);
-}
 
-feedback.innerHTML = ul.join(" ");
+// feedback.innerHTML = ul.join(" ");
