3e18dc09f937aae105a3561b965668b120d56ad5
cheny409 <cheny409@newschool.edu>
2022-05-05 09:25:29 -0400

    Update Project4_Part3.js

--- Project4/Project4_Part3.js
+++ Project4/Project4_Part3.js
@@ -17,8 +17,8 @@ keyboardEventElement.addEventListener('keydown',(event)=>{
     const feedbackElement = document.createElement('img')
     feedbackElement.innerHTML = "Key Pressed:" + event .key
     
-    if(event.key === "b") {
-        const bKeyElement = document.querySelector("#b")
+    if(event.key === "v") {
+        const bKeyElement = document.querySelector("#v")
         bKeyElement.innerHTML = "<img src=\"./Image/squeeze.png\">";
 
     }
@@ -39,6 +39,122 @@ keyboardEventElement.addEventListener('keydown',(event)=>{
     keyboardFeedbackEventElement.appendChild(feedbackElement)
 
     keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "5") {
+        const bKeyElement = document.querySelector("#five")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeeze5.png\">";
+
+    }
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "6") {
+        const bKeyElement = document.querySelector("#six")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeeze6.png\">";
+
+    }
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "7") {
+        const bKeyElement = document.querySelector("#seven")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeeze7.png\">";
+
+    }
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "8") {
+        const bKeyElement = document.querySelector("#eight")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeeze8.png\">";
+
+    }
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "e") {
+        const bKeyElement = document.querySelector("#e")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeezee.png\">";
+
+    }
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+    if(event.key === "r") {
+        const bKeyElement = document.querySelector("#r")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeezer.png\">";
+        }
+        if(event.key === "t") {
+            const bKeyElement = document.querySelector("#t")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezet.png\">";
+            }
+        if(event.key === "t") {
+                const bKeyElement = document.querySelector("#t")
+                bKeyElement.innerHTML = "<img src=\"./Image/squeezet.png\">";
+                }
+    if(event.key === "y") {
+                    const bKeyElement = document.querySelector("#y")
+                    bKeyElement.innerHTML = "<img src=\"./Image/squeezey.png\">";
+                    }
+       if(event.key === "u") {
+      const bKeyElement = document.querySelector("#u")
+      bKeyElement.innerHTML = "<img src=\"./Image/squeezeu.png\">";
+                        }  
+       if(event.key === "i") {
+const bKeyElement = document.querySelector("#i")
+bKeyElement.innerHTML = "<img src=\"./Image/squeezei.png\">";
+                            }  
+      if(event.key === "s") {
+       const bKeyElement = document.querySelector("#s")
+       bKeyElement.innerHTML = "<img src=\"./Image/squeezes.png\">";
+                            }        
+       if(event.key === "d") {
+        const bKeyElement = document.querySelector("#d")
+        bKeyElement.innerHTML = "<img src=\"./Image/squeezed.png\">";
+                             }     
+        if(event.key === "f") {
+           const bKeyElement = document.querySelector("#f")
+           bKeyElement.innerHTML = "<img src=\"./Image/squeezef.png\">";
+                              }
+        if(event.key === "g") {
+            const bKeyElement = document.querySelector("#g")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezeg.png\">";
+                               }
+        if(event.key === "h") {
+           const bKeyElement = document.querySelector("#h")
+           bKeyElement.innerHTML = "<img src=\"./Image/squeezeh.png\">";
+                                }
+        if(event.key === "j") {
+            const bKeyElement = document.querySelector("#j")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezej.png\">";
+                                } 
+        if(event.key === "x") {
+            const bKeyElement = document.querySelector("#x")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezex.png\">";
+                                }
+        if(event.key === "c") {
+            const bKeyElement = document.querySelector("#c")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezec.png\">";
+                                }
+        if(event.key === "b") {
+            const bKeyElement = document.querySelector("#b")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezeb.png\">";
+                                }
+        if(event.key === "n") {
+            const bKeyElement = document.querySelector("#n")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezen.png\">";
+                                }
+        if(event.key === "m") {
+            const bKeyElement = document.querySelector("#m")
+            bKeyElement.innerHTML = "<img src=\"./Image/squeezem.png\">";
+                                }
+        
+                               
+        
+                                                                  
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
+    keyboardFeedbackEventElement.appendChild(feedbackElement)
+
     })
 
 
